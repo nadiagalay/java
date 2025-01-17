c4b1e7b8472364741426b95436f4b13af4e22772
nadiagalay <nadiakidstv@gmail.com>
2025-01-17 12:45:05 +0200

    start

---
+++ js1.js
@@ -0,0 +1,10 @@
+document.getElementById("myfirstButton").onclick = function () {
+    let a=prompt("Enter your name");
+    let b=prompt("Enter another name");
+    console.log(typeof(a));
+    let p=a*b;
+    alert("периметир"+p); 
+
+
+
+}
